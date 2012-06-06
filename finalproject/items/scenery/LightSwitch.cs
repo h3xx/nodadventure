e7d557e48b5cc75b316c0cb23fdf026c9cd31cde
Dan Church <amphetamachine@gmail.com>
2012-06-06 14:07:30 -0500

    Make lightswitch control lighting in the room.

--- finalproject/items/scenery/LightSwitch.cs
+++ finalproject/items/scenery/LightSwitch.cs
@@ -12,14 +12,17 @@ namespace finalproject {
 
 		private int triedPickup = 0;
 		private static List<string> failPickupMsgs = new List<string>() {
-			"You pull the clock from the nightstand and the short cord almost comes unplugged. You decide that the alarm clock is happier where it can get power and put it back.",
-			"You don't think the alarm clock would be happy in your inventory.",
-			"What would be the use of having an unpowered clock?",
+			"You pull on the light switch and it gives you a nasty shock.",
+			"You pull on the light switch and it gives you a nasty shock. Why you keep doing this is beyond me.",
+			"What would be the use of having this item?",
 			"Seriously, you can't pick it up.",
 			"Why don't you try exploring more?",
 		};
 
-		public LightSwitch ()
+		private bool isOn = false;
+		private Closet parent;
+
+		public LightSwitch (Closet whereAmI)
 			: base(
 				"lightswitch",
 			    new List<string>() {
@@ -32,8 +35,28 @@ namespace finalproject {
 			this.CanPickUp = false;
 			this.actionMessages.Add("read", "There's nothing to read on it.");
 			this.actionMessages.Add("look", "You see a small lightswitch.");
+			this.parent = whereAmI;
 		}
 
+		public override string TurnOff () {
+			if (this.isOn) {
+				this.isOn = false;
+				this.parent.DeIlluminate();
+				return "You switch off the light.";
+			} else {
+				return "The light is already off.";
+			}
+		}
+
+		public override string TurnOn () {
+			if (!this.isOn) {
+				this.isOn = true;
+				this.parent.Illuminate();
+				return "You switch on the light.";
+			} else {
+				return "The light is already on.";
+			}
+		}
 		public override string MsgFailPickup () {
 			// cycle through messages
 			return failPickupMsgs[this.triedPickup++ % failPickupMsgs.Count];
