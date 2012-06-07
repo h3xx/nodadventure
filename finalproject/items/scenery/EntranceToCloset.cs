94253024b35469f5dbfe03fd2432899a1630bc27
Dan Church <amphetamachine@gmail.com>
2012-06-07 10:30:44 -0500

    Add closet entrance pseudo-object.
    So you can examine your closet.

---
+++ finalproject/items/scenery/EntranceToCloset.cs
@@ -0,0 +1,44 @@
+/*
+Copyright (C) 2012 Dan Church.
+License GPLv3+: GNU GPL version 3 or later (http://gnu.org/licenses/gpl.html).
+This is free software: you are free to change and redistribute it. There is NO
+WARRANTY, to the extent permitted by law.
+*/
+namespace finalproject {
+	using System;
+	using System.Collections.Generic;
+	class EntranceToCloset
+		: Item {
+
+		private int triedPickup = 0;
+		private static List<string> failPickupMsgs = new List<string>() {
+			"You can't pick that up.",
+			"Seriously, dude, it won't come loose.",
+			"Why don't you try exploring more?",
+		};
+		private static string
+			readString = "There's nothing to read on it.",
+			lookString = "It's a doorway leading to a closet.";
+
+		public EntranceToCloset ()
+			: base(
+				"closet",
+				new List<string>() {
+					"small closet",
+					"door to closet",
+					"doorway to closet",
+				}
+			) {
+
+			this.CanPickUp = false;
+			this.actionMessages.Add("read", readString);
+			this.actionMessages.Add("look", lookString);
+		}
+
+		public override string MsgFailPickup () {
+			// cycle through messages
+			return failPickupMsgs[this.triedPickup++ % failPickupMsgs.Count];
+		}
+	}
+}
+
